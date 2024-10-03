373fda54f7555a2bd3c7f5b0d5a04706cf470cde
smart-life-tech <christlightworld@gmail.com>
2024-10-03 16:10:46 +0100

    modified:   butterflyServo.ino

--- butterflyServo.ino
+++ butterflyServo.ino
@@ -3,10 +3,11 @@
 
 #define SERVOMIN 150 // This is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX 600 // This is the 'maximum' pulse length count (out of 4096)
-
+int smoothDelay = 20;
+int intervalDelay = 500;
 // Initialize the driver with the I2C address
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-void moveServoss(int startServo, int endServo)
+void moveServoForward(int startServo, int endServo)
 {
     // Slowly move all servos from 0 to 180 degrees
     for (int angle = 0; angle <= 180; angle++)
@@ -16,18 +17,7 @@ void moveServoss(int startServo, int endServo)
             int pulse = map(angle, 0, 180, SERVOMIN, SERVOMAX);
             pwm.setPWM(servoNum, 0, pulse);
         }
-        delay(15); // Delay for smooth movement
-    }
-
-    // Reverse the movement (180 to 0 degrees)
-    for (int angle = 180; angle >= 0; angle--)
-    {
-        for (int servoNum = startServo; servoNum <= endServo; servoNum++)
-        {
-            int pulse = map(angle, 0, 180, SERVOMIN, SERVOMAX);
-            pwm.setPWM(servoNum, 0, pulse);
-        }
-        delay(15); // Delay for smooth movement
+        delay(smoothDelay); // Delay for smooth movement
     }
 }
 void setup()
@@ -42,30 +32,30 @@ void setup()
 
 void loop()
 {
-    moveServoss(0, 11);  // Move servos 0 to 11
-    delay(1000);         // Delay for 1 second
-    moveServoss(12, 23); // Move servos 12 to 23
-    delay(1000);         // Delay for 1 second
+    Serial.println("moving servo 1 to 11");
+    moveServoForward(0, 11); // Move servos 0 to 11 to 180 degrees
+    delay(intervalDelay);             // Delay for 1 second
+    Serial.println("moving servo 12 to 23 forward");
+    moveServoForward(12, 23); // Move servos 12 to 23 to 180 degrees
+    delay(intervalDelay);              // Delay for 1 second
+    Serial.println("moving servo 12 to 23");
+    moveServoBackward(12, 23); // Move servos 12 to 23 from 180 degrees
+    delay(intervalDelay);               // Delay for 1 second
+    Serial.println("moving servo 0 to 11");
+    moveServoBackward(0, 11);  // Move servos 0 to 11
+    delay(intervalDelay);               // Delay for 1 second from 180 degrees
 }
 
-void moveServos(int startServo, int endServo)
+void moveServoBackward(int startServo, int endServo)
 {
-    for (int servoNum = startServo; servoNum <= endServo; servoNum++)
+    // Reverse the movement (180 to 0 degrees)
+    for (int angle = 180; angle >= 0; angle--)
     {
-        // Slowly move the servo from 0 to 180 degrees
-        for (int angle = 0; angle <= 180; angle++)
+        for (int servoNum = startServo; servoNum <= endServo; servoNum++)
         {
             int pulse = map(angle, 0, 180, SERVOMIN, SERVOMAX);
             pwm.setPWM(servoNum, 0, pulse);
-            delay(15); // Delay for smooth movement
         }
-
-        // Reverse the movement (180 to 0 degrees)
-        for (int angle = 180; angle >= 0; angle--)
-        {
-            int pulse = map(angle, 0, 180, SERVOMIN, SERVOMAX);
-            pwm.setPWM(servoNum, 0, pulse);
-            delay(15); // Delay for smooth movement
-        }
+        delay(smoothDelay); // Delay for smooth movement
     }
 }
